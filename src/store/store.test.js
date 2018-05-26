712006eacba1bd424ab87effb6c4706639a3920e
Ramon Miklus <r.miklus@sportradar.com>
2018-05-26 18:20:07 +0200

    store test implementation

--- src/store/store.test.js
+++ src/store/store.test.js
@@ -1,13 +1,15 @@
-import store from './index';
+import initStore from './index';
 import { addText } from '../actions';
 
 describe('Basic store tests', () => {
     test('Initial text state must be an empty array', () => {
+        const store = initStore();
         expect(Array.isArray(store.getState().text)).toBeTruthy();
         expect(store.getState().text.length).toEqual(0);
     });
 
     test('Store state array must contain an item more after dispatching ADD_TEXT', () => {
+        const store = initStore();
         const initialTextLength = store.getState().text.length;
         store.dispatch(addText('Hello World'));
 
@@ -15,6 +17,7 @@ describe('Basic store tests', () => {
     });
 
     test('String passed to ADD_TEXT must be the last item contained in text state array', () => {
+        const store = initStore();
         const dummyText = 'Hello World Again!';
         store.dispatch(addText(dummyText));
 
